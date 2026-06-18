532b138db1b5eb47d75579f1f8575ede58d86544
shashankchowdary-921 <shashankchowdary.t2023@vitstudent.ac.in>
2026-06-18 18:52:24 +0530

    Create encoder.py

---
+++ core/encoder.py
@@ -0,0 +1,30 @@
+class ConvolutionalEncoder:
+    def __init__(self):
+        self.reg = [0, 0, 0]
+        self.g1_taps = [0, 1, 2]
+        self.g2_taps = [0, 2]
+    
+    def reset(self):
+        self.reg = [0, 0, 0]
+    
+    def _shift(self, bit: int):
+        self.reg[2] = self.reg[1]
+        self.reg[1] = self.reg[0]
+        self.reg[0] = bit
+    
+    def _compute_output(self, taps):
+        result = 0
+        for tap in taps:
+            result ^= self.reg[tap]
+        return result
+    
+    def encode(self, bits: str) -> str:
+        self.reset()
+        output = []
+        for bit in bits:
+            self._shift(int(bit))
+            out1 = self._compute_output(self.g1_taps)
+            out2 = self._compute_output(self.g2_taps)
+            output.append(str(out1))
+            output.append(str(out2))
+        return ''.join(output)
