89034921075359f89fcf7316a5eede5f70e83e9a
shashankchowdary-921 <shashankchowdary.t2023@vitstudent.ac.in>
2026-06-18 21:09:05 +0530

    Create header.py

---
+++ ui/header.py
@@ -0,0 +1,33 @@
+```python
+import streamlit as st
+
+
+def render_header():
+
+    st.markdown(
+        """
+        <div style="
+            padding-top:10px;
+            padding-bottom:20px;
+        ">
+            <h1 style="
+                margin-bottom:4px;
+                font-size:2.5rem;
+                font-weight:700;
+                color:#111111;
+            ">
+                Convolutional Encoder & Viterbi Decoder
+            </h1>
+
+            <p style="
+                color:#666666;
+                font-size:1rem;
+                margin-top:0px;
+            ">
+                Forward Error Correction Laboratory
+            </p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+```
