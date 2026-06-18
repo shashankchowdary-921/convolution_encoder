17b72389971fb5b56afab483ebe4baddb48b1d68
shashankchowdary-921 <shashankchowdary.t2023@vitstudent.ac.in>
2026-06-18 21:56:43 +0530

    Update header.py

--- ui/header.py
+++ ui/header.py
@@ -4,29 +4,14 @@ import streamlit as st
 
 def render_header():
 
-    st.markdown(
-        """
-        <div style="
-            padding-top:10px;
-            padding-bottom:20px;
-        ">
-            <h1 style="
-                margin-bottom:4px;
-                font-size:2.5rem;
-                font-weight:700;
-                color:#111111;
-            ">
-                Convolutional Encoder & Viterbi Decoder
-            </h1>
+st.markdown("""
+<div style="padding-top:10px;padding-bottom:20px;">
+    <h1 style="margin-bottom:4px;">
+        Convolutional Encoder & Viterbi Decoder
+    </h1>
+    <p style="color:#666666;">
+        Forward Error Correction Laboratory
+    </p>
+</div>
+""", unsafe_allow_html=True)
 
-            <p style="
-                color:#666666;
-                font-size:1rem;
-                margin-top:0px;
-            ">
-                Forward Error Correction Laboratory
-            </p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
