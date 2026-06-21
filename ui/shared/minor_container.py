15a750cf3d35d874fcd3556325707fab424fd95b
shashankchowdary-921 <shashankchowdary.t2023@vitstudent.ac.in>
2026-06-21 16:33:19 +0530

    Update minor_container.py

--- ui/shared/minor_container.py
+++ ui/shared/minor_container.py
@@ -2,33 +2,7 @@ import streamlit as st
 
 def render_minor_container(title, value):
 
-    st.markdown(
-        f"""
-        <div style="
-            border:1px solid #E7E5E4;
-            border-radius:14px;
-            padding:18px;
-            background:#FFFFFF;
-            height:120px;
-        ">
-
-            <div style="
-                color:#6B7280;
-                font-size:0.9rem;
-                margin-bottom:10px;
-            ">
-                {title}
-            </div>
-
-            <div style="
-                color:#111827;
-                font-size:1.5rem;
-                font-weight:600;
-            ">
-                {value}
-            </div>
-
-        </div>
-        """,
-        unsafe_allow_html=True
+    st.metric(
+        label=title,
+        value=value
     )
