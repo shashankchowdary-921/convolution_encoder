6446d448e7477985fdcb2cc905248b2c832e9010
shashankchowdary-921 <shashankchowdary.t2023@vitstudent.ac.in>
2026-06-21 17:21:44 +0530

    Update section.py

--- ui/pipeline/section.py
+++ ui/pipeline/section.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
 
+import streamlit as st
+
+
+def render_stage(
+    title,
+    value
+):
+    with st.container(border=True):
+
+        st.caption(title)
+
+        st.markdown(
+            f"### {value}"
+        )
+
 
 
 
